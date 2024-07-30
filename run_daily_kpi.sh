3473f0d0a3d6b03b06ac1d56dbd06f92a60c115c
lucas richards <lrichards@westridgelabs.com>
2024-07-30 12:00:35 -0700

    backup

--- run_daily_kpi.sh
+++ run_daily_kpi.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Activate the virtual environment
-source /home/lr/path/to/venv/bin/activate
+source /home/bc/path/to/venv/bin/activate
 #/Users/lucasrichards/desktop/projects/westridge_django/westridgeApp/run_collect_kpi.sh
 
 # Navigate to the project directory
@@ -9,3 +9,10 @@ cd ~/westridgeApp
 
 # Run the Django management command
 python3 collect_kpi.py
+
+# backup database
+pg_dumpall > /home/bc/database_backup/backup.sql
+
+# backup all files to backup server ECHO
+date > ~/hotel_backup_trace.txt
+rsync -a /home/bc am@10.1.1.20:/mnt/drobo/systembackup/training_backup_rs
